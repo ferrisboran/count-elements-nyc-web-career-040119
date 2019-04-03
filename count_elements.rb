da9575a397b1e45ae2d62d7ce3478fd198d6de32
ferrisboran <ferris@ferryrules.com>
2019-04-03 18:26:20 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,9 @@
 def count_elements(array)
-  # code goes here
+  new_hash = {}
+  array.each do |index|
+    if !new_hash[index]
+      new_hash[index] = array.count(index)
+    end
+  end
+  new_hash
 end
- 
